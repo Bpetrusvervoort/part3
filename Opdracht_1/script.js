a28ba3ded12110813f7d5f2845d00811aa25936e
Bpetrusvervoort <82579221+Bpetrusvervoort@users.noreply.github.com>
2021-06-24 12:37:32 +0200

    Add files via upload

---
+++ Opdracht_1/script.js
@@ -0,0 +1,13 @@
+console.log("Hello Winc Academy")
+        let name = "Bram";
+        console.log("Mijn naam is Bram");
+        let nummers = 4;
+        console.log("nummers+nummers bij elkaar opgeteld = 8")
+        let strings = "4";
+        console.log("strings+strings bij elkaar opgeteld = 44")
+        let keer = 12;
+        console.log("keer*keer is = 144")
+        let gedeeld = 12;
+        console.log("gedeeld/gedeeld is = 1")
+        let leeftijd = 30;
+        console.log("typeof leeftijd is number")
