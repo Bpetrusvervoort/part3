1615cb3b0b8ce278000ed78601db0efc607f2805
Bpetrusvervoort <82579221+Bpetrusvervoort@users.noreply.github.com>
2021-07-13 11:52:34 +0200

    Add files via upload

---
+++ Opdracht_3/script.js
@@ -0,0 +1,32 @@
+const age = 12; 
+
+if (age > 17 ) {
+    
+    document.write ("You're over 18. You're welcome to come in!");
+
+} else {
+
+    document.write("You're under 18. Please wait outside!");
+}
+
+const isFemale = true; 
+
+if (isFemale) {
+
+    document.write ("You're welcome to join ladies night!");
+} else {
+
+    document.write ("Only ladies are welcome tonight, sorry guys!");
+}
+
+const driverStatus = "bob";
+
+if ("bob") {
+
+    document.write ("Good on you, you're safe driving home!");
+
+} else {
+
+    document.write ("Please hand in your keys and call a cab!");
+
+}
