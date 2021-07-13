1326d7a5a5ea0ff321b4f72315b84f7b0b12e84d
Bpetrusvervoort <82579221+Bpetrusvervoort@users.noreply.github.com>
2021-07-13 10:01:18 +0200

    Add files via upload

---
+++ Opdracht_2/script.js
@@ -0,0 +1,6 @@
+console.log("Hallo Winc Academy studenten")
+// Dit is een grote som
+let sum = 1230941 * 1823794; 
+console.log("sum:", 2244982810154)
+let divide = 637263 / 54;
+console.log("divide:", 11801.166666666666)
