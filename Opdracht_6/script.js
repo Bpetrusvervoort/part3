cbf54f7816539236eeb239ab5105400885240f74
Bpetrusvervoort <82579221+Bpetrusvervoort@users.noreply.github.com>
2021-07-15 13:34:07 +0200

    Add files via upload

---
+++ Opdracht_6/script.js
@@ -0,0 +1,24 @@
+const makePizza = function() {
+    console.log("kneed the pizza dough and make a flat circle")
+    console.log("Spread tomato sauce over the top of the base")
+    console.log("Choose toppings and cover the pizza with them")
+    console.log("Put the pizza in the oven")
+    console.log("Bake for 10minutes until it's golden-brown")
+    console.log("Take the pizza out the oven")
+};
+
+const makeSushi = function() {
+    console.log("cook the rice")
+    console.log("prep vegatables")
+    console.log("wait until the rice is cooled down")
+    console.log("flatten out the rice on seaweed")
+    console.log("Put your vegatables in the middle")
+    console.log("Try to roll the vegatables in the rice and seaweed")
+}
+
+makeSushi();
+makeSushi();
+makeSushi();
+
+makePizza();
+makePizza();
